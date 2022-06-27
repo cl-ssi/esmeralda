2fbea64933d0d2eb027529d9630857140fa8a664
AquaroTorres <atorresf@gmail.com>
2022-06-27 00:47:53 -0400

    Dockerfile

--- config/cache.php
+++ config/cache.php
@@ -44,7 +44,7 @@ return [
 
         'database' => [
             'driver' => 'database',
-            'table' => 'cache',
+            'table' => 'cache_esmeralda',
             'connection' => null,
         ],
 
