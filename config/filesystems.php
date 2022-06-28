2de10050f7352abea7e4622cbc072639c3fe9ead
AquaroTorres <atorresf@gmail.com>
2022-06-28 13:45:50 -0400

    cloud storage spatie config

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => env('FILESYSTEM_DRIVER', 'gcs'),
 
     /*
     |--------------------------------------------------------------------------
@@ -77,15 +77,27 @@ return [
             // 'ssl' => true,
             // 'timeout' => 30,
         ],
+        // 'gcs' => [
+        //     'driver' => 'gcs',
+        //     'project_id' => env('GOOGLE_CLOUD_PROJECT_ID'),
+        //     //'key_file' => env('GOOGLE_CLOUD_SERVICE_ACCOUNT',null), // optional: /path/to/service-account.json
+        //     'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET'),
+        //     //'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
+        //     'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
+        //     'visibility' => 'public', // optional: public|private
+        //     'options' => ['CacheControl' => 'no-store']
+        // ],
+        /** Esto es para el nuevo composer require de spatie */
         'gcs' => [
             'driver' => 'gcs',
-            'project_id' => env('GOOGLE_CLOUD_PROJECT_ID'),
-            //'key_file' => env('GOOGLE_CLOUD_SERVICE_ACCOUNT',null), // optional: /path/to/service-account.json
+            'key_file_path' => env('GOOGLE_CLOUD_KEY_FILE', null), // optional: /path/to/service-account.json
+            // 'key_file' => [], // optional: Array of data that substitutes the .json file (see below)
+            'project_id' => env('GOOGLE_CLOUD_PROJECT_ID'), // optional: is included in key file
             'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET'),
-            //'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
+            'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
             'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
             'visibility' => 'public', // optional: public|private
-            'options' => ['CacheControl' => 'no-store']
+            'metadata' => ['cacheControl'=> 'public,max-age=86400'], // optional: default metadata
         ],
     ],
 
