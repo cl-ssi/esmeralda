a3cfa68902540a07a8558d83479f100efa0bdc62
GermanZun <german.zuniga@gmail.com>
2022-07-04 10:00:24 -0400

    tasks test

--- routes/web.php
+++ routes/web.php
@@ -520,5 +520,5 @@ Route::prefix('pending_patient')->name('pending_patient.')->middleware('auth')->
 
 
 Route::get('/email_queue_test', 'SuspectCaseController@emailQueueTest');
-Route::get('/handle-task ', 'SuspectCaseController@emailQueueTest');
+Route::get('/handle-task ', 'SuspectCaseController@handleTask');
 
