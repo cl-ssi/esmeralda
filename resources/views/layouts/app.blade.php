41aeb8b422622d739f3405bc5eaec890fb4f8d5a
Alvaro Torres F <alvarotorres@gmail.com>
2022-07-10 17:08:37 +0000

    Fix login dv=0

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -511,7 +511,7 @@
 
             @auth
                 @if(!auth()->user()->run OR 
-                    !auth()->user()->dv OR 
+
                     !auth()->user()->name OR 
                     count(preg_split('/\W+/u', auth()->user()->name, -1, PREG_SPLIT_NO_EMPTY)) <= 2 OR
                     !auth()->user()->telephone OR 
