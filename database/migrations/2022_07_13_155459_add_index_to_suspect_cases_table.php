2945295480546adba6bae1405135c6417b686269
tebiccr <tebiccr@gmail.com>
2022-07-13 18:06:53 -0400

    con el indice de laboratorio_pcr

--- database/migrations/2022_07_13_155459_add_index_to_suspect_cases_table.php
+++ database/migrations/2022_07_13_155459_add_index_to_suspect_cases_table.php
@@ -18,6 +18,9 @@ class AddIndexToSuspectCasesTable extends Migration
             $table->index('pcr_sars_cov_2');
             $table->index(['pcr_sars_cov_2', 'sample_at']);
             $table->index(['pcr_sars_cov_2', 'sample_at','establishment_id'], 'pcr_establishment');
+
+            //Indice para examenes de laboratorio
+            $table->index(['pcr_sars_cov_2', 'laboratory_id']);
         });
     }
 
@@ -33,6 +36,9 @@ class AddIndexToSuspectCasesTable extends Migration
             $table->dropIndex('suspect_cases_pcr_sars_cov_2_index');
             $table->dropIndex('suspect_cases_pcr_sars_cov_2_sample_at_index');
             $table->dropIndex('pcr_establishment');
+
+            //Indice para el listado de examenes de laboratorio
+            $table->dropIndex('suspect_cases_pcr_sars_cov_2_laboratory_id_index');
         });
     }
 }
