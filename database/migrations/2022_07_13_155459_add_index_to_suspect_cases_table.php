0c5e88159663ab72f3c0eb9a541dc3edb0950528
tebiccr <tebiccr@gmail.com>
2022-07-13 17:37:14 -0400

    fix con el sample_at en lugar de cov_2_at

--- database/migrations/2022_07_13_155459_add_index_to_suspect_cases_table.php
+++ database/migrations/2022_07_13_155459_add_index_to_suspect_cases_table.php
@@ -16,8 +16,8 @@ class AddIndexToSuspectCasesTable extends Migration
         Schema::table('suspect_cases', function (Blueprint $table) {
             //
             $table->index('pcr_sars_cov_2');
-            $table->index(['pcr_sars_cov_2', 'pcr_sars_cov_2_at']);
-            $table->index(['pcr_sars_cov_2', 'pcr_sars_cov_2_at','establishment_id'], 'pcr_establishment');
+            $table->index(['pcr_sars_cov_2', 'sample_at']);
+            $table->index(['pcr_sars_cov_2', 'sample_at','establishment_id'], 'pcr_establishment');
         });
     }
 
@@ -31,7 +31,7 @@ class AddIndexToSuspectCasesTable extends Migration
         Schema::table('suspect_cases', function (Blueprint $table) {
             //
             $table->dropIndex('suspect_cases_pcr_sars_cov_2_index');
-            $table->dropIndex('suspect_cases_pcr_sars_cov_2_pcr_sars_cov_2_at_index');
+            $table->dropIndex('suspect_cases_pcr_sars_cov_2_sample_at_index');
             $table->dropIndex('pcr_establishment');
         });
     }
