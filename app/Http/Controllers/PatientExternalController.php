c94f4328903f3dabe6f7c1ac155dbe6b12a5bbee
GermanZun <german.zuniga@gmail.com>
2022-09-13 23:45:16 -0300

    fix update al log in cu

--- app/Http/Controllers/PatientExternalController.php
+++ app/Http/Controllers/PatientExternalController.php
@@ -86,9 +86,8 @@ class PatientExternalController extends Controller
             LogAccessCu::create(['patient_id' => $patient->id]);
             
             /** Marcar usuario como logueado por CU y actualizar nombre */
-            $patient->update(['logged_by_cu' => now()]); 
-            
             if($run == '16351236') {
+                $patient->update(['logged_by_cu_at' => now()]); 
                 $patient->updateNameFromCU($userClaveUnica);
             }
             
